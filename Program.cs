42e995473397b7a0c998a288c861db771da509f3
Vadim-Falcon <stepanenko190709@gmail.com>
2025-10-01 20:43:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //Змінна 1
+        Console.WriteLine("Введiть a (long): ");
+
+        long a = long.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введiть x (long): ");
+
+        long x = long.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введiть t (double): ");
+        
+        double t = double.Parse(Console.ReadLine());
+
+        long r1 = (long)(a * x * t);
+
+        long r2 = (long)(a * (a + x) / t);
+
+        double r3 = a / t * (a + x / t);
+
+        Console.WriteLine("result1 = " + r1);
+
+        Console.WriteLine("result2 = " + r2);
+
+        Console.WriteLine("result3 = " + r3.ToString("F5"));
+
+        //Змінна 2
+        Console.WriteLine("Введiть a1 (byte): ");
+
+        byte a1 = byte.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введiть b (double): ");
+
+        double b = double.Parse(Console.ReadLine());
+
+        byte r21 = (byte)(a * b);  
+
+        double r22 = a / b + 7;
+
+        Console.WriteLine("result21 = " + r21);
+
+        Console.WriteLine("result22 = " + r22.ToString("F8"));
+    }
+
+}
